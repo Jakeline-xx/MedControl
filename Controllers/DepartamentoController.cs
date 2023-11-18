bec4061dcebc37d97d453f426859210a0c375223
Jakeline Santana da Rocha <82068580+Jakeline-xx@users.noreply.github.com>
2023-11-18 17:40:47 -0300

    dev: CRUD Departamento

---
+++ Controllers/DepartamentoController.cs
@@ -0,0 +1,104 @@
+﻿using MedControl.Data.Repositories.Abstractions;
+using MedControl.Models;
+using MedControl.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedControl.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        private readonly IDepartamentoRepository _departamentoRepository;
+
+        public DepartamentoController(IDepartamentoRepository departamentoRepository)
+        {
+            _departamentoRepository = departamentoRepository;
+        }
+
+        public IActionResult Index()
+        {
+            var departamentos = _departamentoRepository.ObterTodos();
+            return View(departamentos);
+        }
+
+        public IActionResult Detalhes(Guid id)
+        {
+            var departamento = _departamentoRepository.ObterPorId(id);
+
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(departamento);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Criar(DepartamentoViewModel departamentoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Departamento departamento = departamentoViewModel;
+                _departamentoRepository.Adicionar(departamento);
+                return RedirectToAction("Index");
+            }
+
+            return View(departamentoViewModel);
+        }
+
+        public async Task<IActionResult> Editar(Guid id)
+        {
+            var departamento = await _departamentoRepository.ObterPorId(id);
+
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new DepartamentoViewModel
+            {
+                Nome = departamento.Nome,
+                Descricao = departamento.Descricao
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(DepartamentoViewModel departamentoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Departamento departamento = departamentoViewModel;
+                await _departamentoRepository.Atualizar(departamento);
+                return RedirectToAction("Index");
+            }
+
+            return View(departamentoViewModel);
+        }
+
+        public async Task<IActionResult> Excluir(Guid id)
+        {
+            var departamento = await _departamentoRepository.ObterPorId(id);
+
+            if (departamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(departamento);
+        }
+
+        [HttpPost]
+        public IActionResult ExcluirConfirmado(Guid id)
+        {
+            _departamentoRepository.Remover(id);
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ ViewModels/DepartamentoViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MedControl.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace MedControl.ViewModels
+{
+    public class DepartamentoViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Descrição é obrigatório.")]
+        public string Descricao { get; set; }
+
+        public static implicit operator Departamento(DepartamentoViewModel viewModel)
+        {
+            return new Departamento
+            {
+                Nome = viewModel.Nome,
+                Descricao = viewModel.Descricao
+            };
+        }
+    }
+}
