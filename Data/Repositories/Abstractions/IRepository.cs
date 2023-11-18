01815d2df8d09242f071a7b8576172710ab9c660
Jakeline Santana da Rocha <82068580+Jakeline-xx@users.noreply.github.com>
2023-11-18 16:34:57 -0300

    dev: Generic Repository Pattern

---
+++ Data/Repositories/Abstractions/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using MedControl.Models;
+using System.Linq.Expressions;
+
+namespace MedControl.Data.Repositories.Abstractions
+{
+    public interface IRepository<TEntidade> : IDisposable where TEntidade : Entidade
+    {
+        Task Adicionar(TEntidade entidade);
+        Task<TEntidade> ObterPorId(Guid id);
+        Task<List<TEntidade>> ObterTodos();
+        Task Atualizar(TEntidade entidade);
+        Task Remover(Guid id);
+        Task<IEnumerable<TEntidade>> Buscar(Expression<Func<TEntidade, bool>> predicate);
+        Task<int> SaveChanges();
+    }
+}
---
+++ Data/Repositories/Repository.cs
@@ -0,0 +1,63 @@
+﻿using MedControl.Data.Contexts;
+using MedControl.Data.Repositories.Abstractions;
+using MedControl.Models;
+using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedControl.Data.Repositories
+{
+    public abstract class Repository<TEntidade> : IRepository<TEntidade> where TEntidade : Entidade, new()
+    {
+        protected readonly MedControlDbContext Db;
+        protected readonly DbSet<TEntidade> DbSet;
+
+        protected Repository(MedControlDbContext db)
+        {
+            Db = db;
+            DbSet = db.Set<TEntidade>();
+        }
+
+        public async Task<IEnumerable<TEntidade>> Buscar(Expression<Func<TEntidade, bool>> predicate)
+        {
+            return await DbSet.AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public virtual async Task<TEntidade> ObterPorId(Guid id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public virtual async Task<List<TEntidade>> ObterTodos()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public virtual async Task Adicionar(TEntidade entidade)
+        {
+            DbSet.Add(entidade);
+            await SaveChanges();
+        }
+
+        public virtual async Task Atualizar(TEntidade entidade)
+        {
+            DbSet.Update(entidade);
+            await SaveChanges();
+        }
+
+        public virtual async Task Remover(Guid id)
+        {
+            DbSet.Remove(new TEntidade { Id = id });
+            await SaveChanges();
+        }
+
+        public async Task<int> SaveChanges()
+        {
+            return await Db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            Db?.Dispose();
+        }
+    }
+}
