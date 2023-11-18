1c055b710ba40cf6fba4039dc8c901fa469cff2e
Jakeline Santana da Rocha <82068580+Jakeline-xx@users.noreply.github.com>
2023-11-18 17:52:23 -0300

    dev: CRUD UnidadeTrabalho

---
+++ Controllers/UnidadeTrabalhoController.cs
@@ -0,0 +1,105 @@
+﻿using MedControl.Data.Repositories.Abstractions;
+using MedControl.Models;
+using MedControl.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MedControl.Controllers
+{
+    public class UnidadeTrabalhoController : Controller
+    {
+        private readonly IUnidadeTrabalhoRepository _unidadeTrabalhoRepository;
+
+        public UnidadeTrabalhoController(IUnidadeTrabalhoRepository unidadeTrabalhoRepository)
+        {
+            _unidadeTrabalhoRepository = unidadeTrabalhoRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var unidadesTrabalho = await _unidadeTrabalhoRepository.ObterTodos();
+            return View(unidadesTrabalho);
+        }
+
+        public async Task<IActionResult> Detalhes(Guid id)
+        {
+            var unidadeTrabalho = await _unidadeTrabalhoRepository.ObterPorId(id);
+
+            if (unidadeTrabalho == null)
+            {
+                return NotFound();
+            }
+
+            return View(unidadeTrabalho);
+        }
+
+        public IActionResult Criar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Criar(UnidadeTrabalhoViewModel unidadeTrabalhoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                UnidadeTrabalho unidadeTrabalho = unidadeTrabalhoViewModel;
+                await _unidadeTrabalhoRepository.Adicionar(unidadeTrabalho);
+                return RedirectToAction("Index");
+            }
+
+            return View(unidadeTrabalhoViewModel);
+        }
+
+        public async Task<IActionResult> Editar(Guid id)
+        {
+            var unidadeTrabalho = await _unidadeTrabalhoRepository.ObterPorId(id);
+
+            if (unidadeTrabalho == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = new UnidadeTrabalhoViewModel
+            {
+                Nome = unidadeTrabalho.Nome,
+                Logradouro = unidadeTrabalho.Logradouro
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(UnidadeTrabalhoViewModel unidadeTrabalhoViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                UnidadeTrabalho unidadeTrabalho = unidadeTrabalhoViewModel;
+                await _unidadeTrabalhoRepository.Atualizar(unidadeTrabalho);
+                return RedirectToAction("Index");
+            }
+
+            return View(unidadeTrabalhoViewModel);
+        }
+
+        public async Task<IActionResult> Excluir(Guid id)
+        {
+            var unidadeTrabalho = await _unidadeTrabalhoRepository.ObterPorId(id);
+
+            if (unidadeTrabalho == null)
+            {
+                return NotFound();
+            }
+
+            return View(unidadeTrabalho);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ExcluirConfirmado(Guid id)
+        {
+            await _unidadeTrabalhoRepository.Remover(id);
+            return RedirectToAction("Index");
+        }
+    }
+
+
+}
---
+++ ViewModels/UnidadeTrabalhoViewModel.cs
@@ -0,0 +1,25 @@
+﻿using MedControl.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace MedControl.ViewModels
+{
+    public class UnidadeTrabalhoViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Logradouro é obrigatório.")]
+        public string Logradouro { get; set; }
+
+        public static implicit operator UnidadeTrabalho(UnidadeTrabalhoViewModel viewModel)
+        {
+            return new UnidadeTrabalho
+            {
+                Nome = viewModel.Nome,
+                Logradouro = viewModel.Logradouro
+            };
+        }
+    }
+}
