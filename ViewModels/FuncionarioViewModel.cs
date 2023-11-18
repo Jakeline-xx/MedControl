3a05817281e2cfd1b5dbee1749e14f8fcdddaf8d
Jakeline Santana da Rocha <82068580+Jakeline-xx@users.noreply.github.com>
2023-11-18 17:10:21 -0300

    dev: FuncionarioViewModel

---
+++ ViewModels/FuncionarioViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MedControl.ViewModels
+{
+    public class FuncionarioViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório")]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Cargo é obrigatório")]
+        public string Cargo { get; set; }
+
+        [Required(ErrorMessage = "O campo Identificação é obrigatório")]
+        public string Identificacao { get; set; }
+
+        [Phone(ErrorMessage = "O campo Telefone não é um número de telefone válido")]
+        public string Telefone { get; set; }
+    }
+}
